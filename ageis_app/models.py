beea4056a2612ab5c42182414e0ac839b7097050
achujoseph0709 <achujozefsl0709@gmail.com>
2024-04-24 16:14:20 +0530

    Phone num length

--- ageis_app/models.py
+++ ageis_app/models.py
@@ -6,7 +6,7 @@ class ExtendedUserModel(models.Model):
     def __str__(self):
         return self.user.username
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='extenedusermodel',blank=True,null=True)
-    phone = models.CharField(max_length=10,blank=True,null=True)
+    phone = models.CharField(max_length=20,blank=True,null=True)
     cv = models.FileField(upload_to='CV',blank=True,null=True)
     #####################################
     location = models.CharField(max_length=255, blank=True, null=True)
